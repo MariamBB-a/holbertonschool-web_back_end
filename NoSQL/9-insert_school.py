97f274242c9c89498ed4b057d5c7e9d848ae1c91
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-05-02 19:28:14 +0000

    in doc list collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Insert a new document in a MongoDB collection
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into the collection using kwargs.
+
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: document fields
+
+    Returns:
+        The new document _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
