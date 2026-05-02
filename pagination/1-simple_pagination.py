6b8f8c2d8a47e747f65eb844fc31ca88fefd65b1
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-05-02 19:08:25 +0000

    pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,8 +36,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Return the appropriate page of the dataset.
         """
