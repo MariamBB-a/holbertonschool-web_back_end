f50ac526e13bfd4f327c13fbfd56395487974535
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-05-02 19:11:02 +0000

    pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -37,11 +37,7 @@ class Server:
 
         return self.__indexed_dataset
 
-    def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-    ) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Return a dictionary with deletion-resilient pagination.
         """
