1c5c8b9e1e1774b2315f46628de0aa02edda618e
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-05-14 12:32:34 +0200

    list student

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,16 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (
+    typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
 
   return [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter(
+      (value) => typeof value === 'string'
+        && value.startsWith(startString)
+    )
     .map((value) => value.slice(startString.length))
     .join('-');
 }
