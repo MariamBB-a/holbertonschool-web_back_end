f0bdf6aa174e7cecb3f1affbeaf9eed4f43469d1
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-06-10 09:50:20 +0200

    login to concle

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-const process = require('process');
-
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
