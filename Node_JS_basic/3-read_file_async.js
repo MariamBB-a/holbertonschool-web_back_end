8784606675c58c3b4e7c9f052df33a10a734e611
‘MariamBB-a’ <mariam.backroush@gmail.com>
2026-06-10 09:59:13 +0200

    read data base for number of students

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -19,7 +19,9 @@ function countStudents(path) {
       const fields = {};
 
       students.forEach((student) => {
-        const [firstname, , , field] = student.split(',');
+        const parts = student.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
 
         if (!fields[field]) {
           fields[field] = [];
@@ -30,7 +32,7 @@ function countStudents(path) {
 
       Object.keys(fields).forEach((field) => {
         console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
         );
       });
 
